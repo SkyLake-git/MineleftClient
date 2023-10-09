d0d7b1c308de051a11e4140f69bc519124767f8f
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-10-10 01:54:42 +0900

    チャンクセンディングメソッドの変更に対応

--- src/network/protocol/PacketPlayerAuthInput.php
+++ src/network/protocol/PacketPlayerAuthInput.php
@@ -7,6 +7,7 @@ namespace Lyrica0954\Mineleft\network\protocol;
 use Lyrica0954\Mineleft\net\PacketBounds;
 use Lyrica0954\Mineleft\network\protocol\types\InputData;
 use Lyrica0954\Mineleft\utils\BinaryUtils;
+use pocketmine\math\Vector3;
 use pocketmine\utils\BinaryStream;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
@@ -17,6 +18,13 @@ class PacketPlayerAuthInput extends MineleftPacket {
 
 	public InputData $inputData;
 
+	public Vector3 $requestedPosition;
+
+	/**
+	 * @var array<int, int>
+	 */
+	public array $nearbyBlocks;
+
 	public function getProtocolId(): int {
 		return ProtocolIds::PLAYER_AUTH_INPUT;
 	}
@@ -25,6 +33,18 @@ class PacketPlayerAuthInput extends MineleftPacket {
 		BinaryUtils::putString($out, $this->playerUuid->toString());
 
 		$this->inputData->write($out);
+		BinaryUtils::putVec3d($out, $this->requestedPosition);
+
+		$withNearbyBlocks = count($this->nearbyBlocks) > 0;
+
+		$out->putBool($withNearbyBlocks);
+		if ($withNearbyBlocks) {
+			$out->putInt(count($this->nearbyBlocks));
+			foreach ($this->nearbyBlocks as $code => $block) {
+				$out->putLong($code);
+				$out->putInt($block);
+			}
+		}
 	}
 
 	public function decode(BinaryStream $in): void {
@@ -32,8 +52,18 @@ class PacketPlayerAuthInput extends MineleftPacket {
 
 		$inputData = new InputData();
 		$inputData->read($in);
-		
+
 		$this->inputData = $inputData;
+		$this->requestedPosition = BinaryUtils::getVec3d($in);
+
+		$this->nearbyBlocks = [];
+		$withNearbyBlocks = $in->getBool();
+		if ($withNearbyBlocks) {
+			$count = $in->getInt();
+			for ($i = 0; $i < $count; $i++) {
+				$this->nearbyBlocks[$in->getLong()] = $in->getInt();
+			}
+		}
 	}
 
 	public function bounds(): PacketBounds {
