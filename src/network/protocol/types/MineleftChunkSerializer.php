e443fc3ccbcfecbe9104a0eb31f940ef941bb50a
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-10-10 02:01:26 +0900

    不要なパラメーターの削除

--- src/network/protocol/types/MineleftChunkSerializer.php
+++ src/network/protocol/types/MineleftChunkSerializer.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Lyrica0954\Mineleft\network\protocol\types;
 
+use pocketmine\math\Vector3;
 use pocketmine\network\mcpe\convert\TypeConverter;
 use pocketmine\utils\BinaryStream;
 use pocketmine\world\format\Chunk;
@@ -11,29 +12,29 @@ use pocketmine\world\format\SubChunk;
 
 class MineleftChunkSerializer {
 
-	public static function serialize(Chunk $chunk, int $chunkX, int $chunkY): string {
+	public static function serialize(Chunk $chunk): string {
 		$out = new BinaryStream();
 		$out->putInt(Chunk::MAX_SUBCHUNKS);
 
 		foreach ($chunk->getSubChunks() as $y => $subChunk) {
 			$out->putInt($y);
 
-			$data = self::serializeSubChunk($subChunk, $chunkX, $chunkY);
+			$data = self::serializeSubChunk($subChunk);
 			$out->put($data);
 		}
 
 		return $out->getBuffer();
 	}
 
-	public static function serializeSubChunk(SubChunk $subChunk, int $chunkX, int $chunkY): string {
+	public static function serializeSubChunk(SubChunk $subChunk): string {
 		$out = new BinaryStream();
 
 		$out->putInt(count($subChunk->getBlockLayers()));
 		foreach ($subChunk->getBlockLayers() as $layer) {
 			$out->putInt(SubChunk::EDGE_LENGTH ** 3);
-			for ($cx = 0; $cx < SubChunk::EDGE_LENGTH; $cx++) {
-				for ($cy = 0; $cy < SubChunk::EDGE_LENGTH; $cy++) {
-					for ($cz = 0; $cz < SubChunk::EDGE_LENGTH; $cz++) {
+			for ($cx = 0; $cx < SubChunk::EDGE_LENGTH; ++$cx) {
+				for ($cy = 0; $cy < SubChunk::EDGE_LENGTH; ++$cy) {
+					for ($cz = 0; $cz < SubChunk::EDGE_LENGTH; ++$cz) {
 						$block = $layer->get($cx, $cy, $cz);
 						$out->putLong(morton3d_encode($cx, $cy, $cz));
 						$networkId = TypeConverter::getInstance()->getBlockTranslator()->internalIdToNetworkId($block);
@@ -46,8 +47,27 @@ class MineleftChunkSerializer {
 		return $out->getBuffer();
 	}
 
-	public static function serializeBlock(): string {
+	/**
+	 * @param array{
+	 *     0: Vector3,
+	 *     1: int
+	 * } $blocks
+	 * @return string
+	 */
+	public static function convertBlocks(array $blocks): string {
+		$out = new BinaryStream();
 
+		$out->putInt(count($blocks));
+
+		foreach ($blocks as $data) {
+			[$pos, $stateId] = $data;
+
+			$out->putLong(morton3d_encode($pos->x, $pos->y, $pos->z));
+			$networkId = TypeConverter::getInstance()->getBlockTranslator()->internalIdToNetworkId($stateId);
+			$out->putInt($networkId);
+		}
+
+		return $out->getBuffer();
 	}
 
 }
